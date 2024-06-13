fef81f0157b8e131ed2ab6accdebbef9bd478152
Zeya Imran <khanzeya63@gmail.com>
2024-06-13 22:06:15 +0530

    setup complete with shadcn

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>welcome in admin panel with shadCn</h1>
+        <Button>ShadCn Button</Button>     
+      </div>
+    </>
+  )
+}
+
+export default App
