7e526a5dbef0e5da9952bf948446edd477602bff
Zeya Imran <khanzeya63@gmail.com>
2024-06-14 01:20:11 +0530

    add dashboard layout

---
+++ src/pages/Books.tsx
@@ -0,0 +1,15 @@
+const Books = () => {
+  return (
+    <>
+      <div className="flex items-center">
+        <h1 className="text-lg font-semibold md:text-2xl">Books</h1>
+      </div>
+      <div
+        className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+        x-chunk="dashboard-02-chunk-1"
+      ></div>
+    </>
+  );
+};
+
+export default Books;
--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import Layout from "./pages/layout";
+import Books from "./pages/Books";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Homepage />,
+    path: "dashboard",
+    element: <Layout />,
+    children: [
+      {
+        path: "home",
+        element: <Homepage />,
+      },
+      {
+        path: "books",
+        element: <Books />,
+      },
+    ],
   },
   {
     path: "/signin",
